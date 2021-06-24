75d097510159a04d4621ee7a87877d755e62e122
AhyoungOh <ahyoung.oh@stonybrook.edu>
2021-06-24 21:50:37 +0900

    gi

---
+++ server/src/models/board/index.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose');
+const BoardSchema = new Schema({
+  id: {
+    type: Number,
+  },
+  title: {
+    type: String,
+  },
+  content: {
+    type: String,
+  },
+});
+module.exports = model('board', BoardSchema);
