77149b5d931e9748b05af8f4128b622169e98a5a
AhyoungOh <ahyoung.oh@stonybrook.edu>
2021-07-03 10:19:00 +0900

    login-error2

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,6 +1,4 @@
 import Board from '../../components/Board';
-// import Header from '../../components/Header';
-// import Footer from '../../components/Footer';
 import Write from '../../components/Write';
 import Detail from '../../components/Detail';
 import { Router, useHistory, useLocation } from 'react-router';
@@ -12,13 +10,6 @@ import { useState } from 'react';
 function Main() {
   const history = useHistory();
   const location = useLocation();
-  // const titleName = '홍당무마켓';
-  // const Headercompoents = <Header titleName={titleName} />;
-  // const [boardData, setBoardData] = useState(null);
-  // const [title, setTitle] = useState(boardData?.title ?? '');
-  // const [link, setLink] = useState(boardData?.link ?? '');
-  // const [category, setCategory] = useState(boardData?.category ?? '');
-  // const [contents, setContents] = useState(boardData?.contents ?? '');
   const [visible, setVisible] = useState(false);
   const [loading, testData, error, fetchData] = useApiCall(
     'http://localhost:4000/api/board'
@@ -33,98 +24,6 @@ function Main() {
   if (error !== null) {
     return <div>Error</div>;
   }
-
-  const buttonList = [
-    {
-      title: '홈',
-      color: 'green',
-    },
-    {
-      title: '검색',
-      color: 'yellow',
-    },
-    {
-      title: '내글',
-      color: 'pink',
-    },
-  ];
-
-  const writerNameList = [
-    {
-      title: '제목',
-      onChange: (e) => {
-        setTitle(e.target.value);
-      },
-      value: title,
-    },
-    {
-      title: '사진 링크',
-      onChange: (e) => {
-        setLink(e.target.value);
-      },
-      value: link,
-    },
-    {
-      title: '카테고리',
-      onChange: (e) => {
-        setCategory(e.target.value);
-      },
-      value: category,
-    },
-    {
-      title: '내용',
-      onChange: (e) => {
-        setContents(e.target.value);
-      },
-      value: contents,
-    },
-  ];
-  // const addBoard = () => {
-  //   //add
-  //   setTestData((state) => {
-  //     return [
-  //       ...state,
-  //       {
-  //         id: state.length + 1,
-  //         title,
-  //         category,
-  //         time: 1,
-  //         money: 10000,
-  //         user: "user1",
-  //         imageLink: link,
-  //         contents,
-  //       },
-  //     ];
-  //   });
-  // };
-  // const updateBoard = () => {
-  //   //update
-  //   setTestData((state) => {
-  //     const id = boardData.id;
-  //     const newState = state.map((board) => {
-  //       if (board.id !== id) {
-  //         return board;
-  //       } else {
-  //         return {
-  //           title: title,
-  //           category: category,
-  //           time: 1,
-  //           money: 10000,
-  //           user: "user2",
-  //           imageLink: link,
-  //           contents: contents,
-  //         };
-  //       }
-  //     });
-  //     return newState;
-  //   });
-  //   setVisible(false);
-  // };
-  const buttonNameList = [
-    {
-      title: boardData ? 'update' : 'done',
-    },
-  ];
 
   const BoardComponents = testData.map((boardData) => {
     return (
@@ -144,7 +43,6 @@ function Main() {
   });
   return (
     <div>
-      {Headercompoents}
       {boardData === null ? (
         BoardComponents
       ) : (
@@ -156,7 +54,6 @@ function Main() {
         />
       )}
 
-      <Footer buttonList={buttonList} />
       <button
         className='open-button'
         onClick={() => setVisible((state) => !state)}
@@ -169,8 +66,6 @@ function Main() {
           setData={() => {}}
           fetchData={fetchData}
           setVisible={setVisible}
-          writerNameList={writerNameList}
-          buttonNameList={buttonNameList}
         />
       ) : null}
     </div>
